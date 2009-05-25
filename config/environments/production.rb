af438f6548d928db4ba3ae68344536fd7e5d7a82
Ryan Rempel <rgrempel@gmail.com>
2009-05-25 00:55:08 -0500

    Configure mail to be sent via sendmail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,8 @@ config.action_view.cache_template_loading            = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host = "http://assets.example.com"
 
+config.action_mailer.delivery_method = :sendmail
+
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
